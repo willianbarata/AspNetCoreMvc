6575f1a65ce1d3d4d58d6299039e424b834e6f49
Willian Barata <willianbarata@gmail.com>
2021-10-04 22:43:19 -0300

    Incluindo RequestDelegate e separando os métodos

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -19,19 +19,32 @@ namespace Alura.ListaLeitura.App
             return context.Response.WriteAsync(_repo.ParaLer.ToString());
         }
 
+        public Task LivrosLidos(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lidos.ToString());
+        }
+
+        public Task LivrosLendo(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.Lendo.ToString());
+        }
+
         public Task Roteamento(HttpContext context)
         {
             var _repo = new LivroRepositorioCSV();
-            var caminhosAtendidos = new Dictionary<string, string>
+            var caminhosAtendidos = new Dictionary<string, RequestDelegate>
             {
-                {"/livros/ParaLer", _repo.ParaLer.ToString()},
-                {"/livros/Lendo", _repo.Lendo.ToString()},
-                {"/livros/Lidos", _repo.Lidos.ToString()}
+                {"/livros/ParaLer", LivrosParaLer },
+                {"/livros/Lendo", LivrosLendo},
+                {"/livros/Lidos", LivrosLidos}
             };
 
             if(caminhosAtendidos.ContainsKey(context.Request.Path))
             {
-                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+                var metodo = caminhosAtendidos[context.Request.Path];
+                return metodo.Invoke(context);
             }
             return context.Response.WriteAsync("Caminho Inexistente");
         }
