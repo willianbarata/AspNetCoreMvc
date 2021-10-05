eb7990089c4b36ec1cc51560fc542030189de090
Willian Barata <willianbarata@gmail.com>
2021-10-04 22:11:32 -0300

    Mvc Rudimentar

---
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,41 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(Roteamento); 
+        }
+
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+
+        public Task Roteamento(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            var caminhosAtendidos = new Dictionary<string, string>
+            {
+                {"/livros/ParaLer", _repo.ParaLer.ToString()},
+                {"/livros/Lendo", _repo.Lendo.ToString()},
+                {"/livros/Lidos", _repo.Lidos.ToString()}
+            };
+
+            if(caminhosAtendidos.ContainsKey(context.Request.Path))
+            {
+                return context.Response.WriteAsync(caminhosAtendidos[context.Request.Path]);
+            }
+            return context.Response.WriteAsync("Caminho Inexistente");
+        }
+
+
+    }
+}
