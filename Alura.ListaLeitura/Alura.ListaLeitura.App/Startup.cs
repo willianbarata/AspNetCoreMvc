0a364d024e7cffc07cec57967d9751d6f4a01697
Willian Barata <willianbarata@gmail.com>
2021-10-06 21:16:43 -0300

    Incluindo através do formulário

--- Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
+++ Alura.ListaLeitura/Alura.ListaLeitura.App/Startup.cs
@@ -26,19 +26,32 @@ namespace Alura.ListaLeitura.App
             builder.MapRoute("Cadastro/NovoLivro/{nome}/{autor}", NovoLivroParaLer);
             builder.MapRoute("Livros/Detalhes/{id:int}", ExibirDetalhes);
             builder.MapRoute("Cadastro/NovoLivro", ExibeFormulario);
-            
+            builder.MapRoute("Cadastro/Incluir", ProcessaFormulario);
+
             var rotas = builder.Build();
             app.UseRouter(rotas);
             //app.Run(Roteamento); 
+        }
+
+        private Task ProcessaFormulario(HttpContext context)
+        {
+            var livro = new Livro()
+            {
+                Titulo = context.Request.Query["titulo"].First(),
+                Autor = context.Request.Query["autor"].First()
+            };
+            var repo = new LivroRepositorioCSV();
+            repo.Incluir(livro);
+            return context.Response.WriteAsync("O livro foi adicionado com sucesso");
         }
 
         private Task ExibeFormulario(HttpContext context)
         {
             var html = @"
                   <html>
-                     <form>
-                         <input/>
-                         <input/>
+                     <form action='/Cadastro/Incluir'>
+                         <input name='titulo'/>
+                         <input name='autor'/>
                          <button> Gravar </button>
                      </form>
                   </html>
